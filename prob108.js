986220941f3841187bcc53a43632b54187bf1e08
Joe McCourt <joe@aviary.com>
2014-01-06 23:00:34 -0500

    Solved 110, although it took an unreasonably long time

--- prob108.js
+++ prob108.js
@@ -22,12 +22,12 @@
 
 //Note these numbers all have many primefactors
 //Search in order of prime factors instead of numerical value
-
-var primes = [2,3,5,7,11,13,17,19,23,29];
-var pVector = [1,0,0,0,0,0,0,0,0,0];
+var primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59];
+var pVector = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
 var evalVector = function(v) {
-	return Math.pow(2,v[0])*Math.pow(3,v[1])*Math.pow(5,v[2])*Math.pow(7,v[3])*Math.pow(11,v[4])*Math.pow(13,v[5])*Math.pow(17,v[6])*Math.pow(19,v[7])*Math.pow(23,v[8])*Math.pow(29,v[9]);
+	return Math.pow(2,v[0])*Math.pow(3,v[1])*Math.pow(5,v[2])*Math.pow(7,v[3])*Math.pow(11,v[4])*Math.pow(13,v[5])*Math.pow(17,v[6])*Math.pow(19,v[7])*Math.pow(23,v[8])*
+	Math.pow(29,v[9])*Math.pow(31,v[10])*Math.pow(37,v[11])*Math.pow(41,v[12])*Math.pow(43,v[13])*Math.pow(47,v[14])*Math.pow(53,v[15])*Math.pow(59,v[16]);
 };
 
 // n, num solutions, factorization
@@ -64,15 +64,16 @@ var evalVector = function(v) {
 
 //Gen numbers to search
 var primesL = primes.length;
-var maxPower = 10;
-var maxValue = 20000000;
+var maxPower = 20;
+var maxValue = 200000000000000000;
+var maxSolutions = 4000000;
 
 var nArray = [];
 
 var genArray = function(index,v) {
 	var n = evalVector(v);
 	if(n < maxValue) {
-		nArray.push(n);
+		nArray.push({value:n,vec:v});
 
 		for(var i = index; i < primesL; i++) {
 			if(v[i] < maxPower) {
@@ -91,38 +92,127 @@ console.log(nArray.length)
 
 // Sort array of numbers to check
 nArray.sort(function(a, b) {
-	return a - b;
+	return a.value - b.value;
 });
 
 //This function could be speed up
 //Just not sure how yet hmmmm....
+
+// 6 1 42 7:2^4*3
+// 6 2 24 2^3:2^3*3
+// 6 3 18 3^2:2*3^2
+// 6 4 15 2*5:3*5
+// 6 6 12 2^2*3:2^2*3
+
+//19947543780003840000 4350308
+//139885119141768000 4086183
+
+// x-n can't be relatively prime to n
+
 var numSolutions = function(n) {
 	var solutions = 0;
+	var lastX = 0;
 	for(var x = n+1; x <= 2*n; x++) {
 		// var y = x*n/(x-n);
+		console.log(x,(x*n),(x-n));
 		if((x*n)%(x-n)==0) {
 			solutions++;
-			// console.log(n,x,x*n/(x-n))
+			console.log(x,x*n/(x-n));
+			lastX = x;
 		}
 	}
 	return solutions;
 };
 
-// numSolutions(18378360);
+maxPower *= 2;
+var numSolutionsV2 = function(n,index,v) {
+	var sum = 1;
+	if(evalVector(v) > evalVector(n)){return 0;}
+	// console.log(evalVector(n),evalVector(v))
+	for(var i = index; i < primesL; i++) {
+		if(v[i] < maxPower) {
+			var vPass = v.slice(0);
+			vPass[i]++;
+			if(vPass[i] <= 2*n[i]) {
+				sum += numSolutionsV2(n,i,vPass);
+			}
+		}
+	}
+	return sum;
+};
+
+var numSolutionsV3 = function(n,index,v,nVal,vVal) {
+	var sum = 1;
+	// if(vVal > nVal){
+	// // console.log(n,v)
+	// return 0;}
+	 for(var i = index; i < primesL; i++) {
+		if(v[i] < maxPower) {
+			var vPass = [v[0],v[1],v[2],v[3],v[4],v[5],v[6],v[7],v[8],v[9]];
+			vPass[i]++;
+
+			if(vVal*primes[i] > nVal) {
+				break;
+			}
+
+			if(vPass[i] <= 2*n[i]) {
+				sum += numSolutionsV3(n,i,vPass,nVal,vVal*primes[i]);
+			}
+		}
+	}
+	return sum;
+};
+
+
+var numSolutionsV4 = function(n,index,v,n_v) {
+	var sum = 1;
+	 for(var i = index; i < primesL; i++) {
+		if(v[i] < maxPower) {
+			var vPass = [v[0],v[1],v[2],v[3],v[4],v[5],v[6],v[7],v[8],v[9],v[10],v[11],v[12],v[13],v[14],v[15],v[16]];
+			vPass[i]++;
+
+			if(n_v/primes[i] < 1) {
+				break;
+			}
+
+			if(vPass[i] <= 2*n[i]) {
+				sum += numSolutionsV4(n,i,vPass,n_v/primes[i]);
+			}
+		}
+	}
+	return sum;
+};
+
+var pVector = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+// console.log(numSolutions(18378360)); //5954
+// console.log(numSolutions(120)); //32
+//console.log(numSolutionsV3([3,1,1,0,0,0,0,0,0,0],0,[0,0,0,0,0,0,0,0,0,0]));
+
+// console.log(nArray)
 var mostSolutions = 0;
-for(var nIndex = 0; nIndex < nArray.length; nIndex++) {
+var minIndex = 11765;
+// var minIndex = 18860;//17982; //Found by multiple runs
+
+// var n = nArray[18860];
+// console.log(numSolutionsV2(n.vec,0,[0,0,0,0,0,0,0,0,0,0]));
+// console.log(n.value,n.vec); //139885119141768000 [ 6, 4, 3, 2, 2, 2, 1, 1, 1, 1 ]
+
+//18860 139885119141768000 4146188
+for(var nIndex = minIndex; nIndex < nArray.length; nIndex++) {
 	var n = nArray[nIndex];
-	var solutions = numSolutions(n);
+	// var solutions = numSolutions(n);
+	//16399:3253163
+	var solutions = numSolutionsV4(n.vec,0,[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],n.value);
+
 	if(solutions > mostSolutions) {
 		mostSolutions = solutions;
-		console.log(n,solutions);
+		console.log(nIndex,n.value,solutions,n.vec);
 
-		if(mostSolutions > 1000) {
+		if(mostSolutions > maxSolutions) {
 			break;
 		}
 	}
 }
 
-console.log(solutions);
-
-
+//console.log(solutions);
