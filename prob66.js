f529d0a303db8eb3b99fdad62c570cf324bc95eb
Joe McCourt <joe@aviary.com>
2014-02-15 00:08:10 -0500

    More work on 66 again

--- prob66.js
+++ prob66.js
@@ -26,26 +26,75 @@
 // y^2 = (x^2 - 1)/D
 // y = sqrt(x^2-1)/sqrt(D)
 // x = sqrt(1+D*y*y)
-// 
 
-// var sqrts = {};
-// var genSqrts = function(n) {
-// 	for(var i = 0; i < n; i++) {
-// 		sqrts[i*i] = i;
+// 2logx = log(1+d*y*y)
+
+// D * square must be a square minus 1
+// D = (square - 1) / square
+// D = (x*x-1) / (y*y)
+// D = (x/y)^2 - (1/y)^2
+
+// x*x - 1 = x*(x-1)+x-1 = (x-1)*(x+1)
+// y*y = (x-1)(x+1)/D
+//
+
+// var Dmax = 100;
+// var Dx = [];
+// for(var x = 1; x < 100000; x++) {
+// 	for(var di = 2; di < Dmax; di++) {
+// 		// var y = (x*x-1) / (i*i);
+		
+// 		if(Math.sqrt(di) == Math.floor(Math.sqrt(di))) {}
+
+// 		var y = Math.sqrt((x*x+1)/d)
+		
+// 		if(D > Dmax) {break;}
+// 		if(D == Math.floor(D)) {
+// 			if(!Dx[D] || i < Dx[D]) {
+// 				Dx[D] = i;
+// 				//console.log(D,i,j);
+// 			}
+// 		}
 // 	}
-// };
+// }
 
-// genSqrts(100000);
+// console.log(Dx);
+
+function getPrimeFactors(number){
+	var factors = {};
+	var d = 2;
+	var limit = Math.ceil(Math.sqrt(number));
+
+	while(d <= limit){
+		if(!(number%d)){
+			if(!factors[d]){
+				factors[d] = 1;
+			}else{
+				factors[d]++; 
+			}
+			number/=d;
+		}else{
+			d++;
+		}
+	}
+
+	if(d <= number) {
+		factors[number] = 1;
+	}
+	return factors;
+};
 
 var maxX = 1;
 var maxD = 2;
-for(var d = 2; d <= 100; d++) {
+for(var d = 2; d <= 60; d++) {
 	if(Math.sqrt(d)-Math.floor(Math.sqrt(d)) == 0){ continue; }
 
+	var dFactors = getPrimeFactors(d);
+	
 	var x = 2;
 	var y = 1;
-	while(true && y*y*d < 9.0071993e+15) {
-
+	while(true) {
+		// var dFactors = dFactors0.slice(0);
 		// x*x = y*y*d+1;
 
 		// y*y = x*x/d - 1/d
@@ -56,24 +105,54 @@ for(var d = 2; d <= 100; d++) {
 
 		// var x2 = d*y*y+1;
 
-		var y = Math.sqrt((x*x+1)/d);
-		if(x-(x|0)==0) {
+		// x = Math.sqrt(d*y*y+1);
+		// var y = Math.sqrt((x*x+1)/d);
+
+		var xP = x+1;
+		var xM = x-1;
+		var valid = true;
+		for(key in dFactors) {
+			var tmp = dFactors[key];
+			while(tmp) {
+				if(xP % key == 0) {
+					tmp--;
+					xP /= key;
+				} else if(xM % key == 0) {
+					tmp--;
+					xM /= key;
+				} else {
+					x+=Math.min((x+1)/xP*(key-(xP%key)),(x-1)/xM*(key-(xM%key)));
+					valid = false;
+					break;
+				}
+			}
+			if(!valid) { break; }
+		}
+
+		if(valid && Math.sqrt(xP*xM) != Math.floor(Math.sqrt(xP*xM))) {
+			valid = false;
+
+			x++;
+			// x = Math.floor(Math.sqrt(d*Math.pow(Math.ceil(Math.sqrt(xP*xM)),2)));
+		}
+
+		if(valid) {
+			console.log(d,x,xP*xM);
 			if(x > maxX) {
 				maxX = x;
 				maxD = d;
-				console.log(d,x,y);
 			}
 			break;
 		}
-		y++;
-	}
 
-	if(y*y*d >= 9.0071993e+15) {
-		console.log("hit integer limit at",d,x)
+		if(x*x >= 9.0071993e+13) {
+			console.log("hit integer limit at",d,x)
+			break;
+		}
 	}
 }
 
-
+console.log(maxX);
 
 
 
