f6f322debabf94e0e33f242cb9ef1181db45e51d
Joe McCourt <joe.j.mccourt@gmail.com>
2015-06-24 21:33:09 -0400

    Call any problem using command line input

--- go/main.go
+++ go/main.go
@@ -1,6 +1,34 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
-	prob14()
+func main() {
+	problems := map[string]interface{}{
+		"1": prob1,
+		"2": prob2,
+		"3": prob3,
+		"4": prob4,
+		"5": prob5,
+		"6": prob6,
+		"7": prob7,
+		"8": prob8,
+		"9": prob9,
+		"10": prob10,
+		"11": prob11,
+		"12": prob12,
+		"13": prob13,
+		"14": prob14,
+	}
+
+	prob := "14"
+	if len(os.Args) > 1 {
+		prob = os.Args[1]
+	}
+
+	fmt.Println("Running problem:", prob)
+
+	problems[prob].(func())()
 }
